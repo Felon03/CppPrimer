036b9192ecc83b58419b0fe1631ad3965f962800
FreedomLy <freedom.jfl@gmail.com>
2017-06-01 21:22:30 +0800

    update 1.8

--- 1.8.cpp
+++ 1.8.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+ï»¿#include<iostream>
 using namespace std;
 
 int main3()
 {
 	cout << "/*" << endl;
 	cout << "*/" << endl;
-	//cout <</*"*/"*/<<endl;				// ´íÎó
-	// ÐÞ¸ÄÎª
+	//cout <</*"*/"*/<<endl;				// é”™è¯¯
+	// ä¿®æ”¹ä¸º
 	cout << "/**/" << endl;
 	cout <</*"*/" /*"/*"*/ << endl;
 	return 0;
